b53ea212b9fa4e9a4bede6fec2efd5977c025875
Schouten <nick.schouten@capgemini.com>
2020-04-22 15:10:39 +0200

    Feedback deel wordt nu ook uit het gesprek gehaald

--- app.py
+++ app.py
@@ -2,13 +2,17 @@ from flask import Flask, request, jsonify, json
 
 app = Flask(__name__)
 
-details = {'AlgemeneInformatie' : {}}
+details = {'AlgemeneInformatie' : {}, 'Feedback' : {}}
 
 @app.route('/webhook', methods=['GET', 'POST'])
 def webhook():
     req = request.get_json(force=True)
-    details.get('AlgemeneInformatie').update(req.get('queryResult').get('parameters'))
-    print(details)
+    parameter = req.get('queryResult').get('parameters')
+    for key in parameter.keys():
+        if key == "gebruiker" or key == "beoordeelde" or key == "project" or key == "begindatum" or key == "einddatum" or key == "rolgebruiker" or key == "rolbeoordeelde" or key == "relatie":
+            details.get('AlgemeneInformatie').update(parameter)
+        elif key == "sterkpunt" or key == "redensterkpunt" or key == "verbeterpunt" or key == "redenverbeterpunt" or key == "overall":
+            details.get('Feedback').update(parameter)
     with open('details.json', 'w') as json_file:
         json.dump(details, json_file)
     return details
