ef708db9925b5bcac08f684e76b1704f86a07a26
Schouten <nick.schouten@capgemini.com>
2020-04-24 12:31:03 +0200

    Comments toegevoegd en vervolgvragen laten sturen als er informatie mist, werkt ineens niet meer

--- app.py
+++ app.py
@@ -6,13 +6,27 @@ details = {'AlgemeneInformatie' : {}, 'Feedback' : {}}
 
 @app.route('/webhook', methods=['GET', 'POST'])
 def webhook():
+    #Get conversation context
     req = request.get_json(force=True)
     parameter = req.get('queryResult').get('parameters')
+    
+    #Filling JSON
     for key in parameter.keys():
         if key == "gebruiker" or key == "beoordeelde" or key == "project" or key == "begindatum" or key == "einddatum" or key == "rolgebruiker" or key == "rolbeoordeelde" or key == "relatie":
             details.get('AlgemeneInformatie').update(parameter)
         elif key == "sterkpunt" or key == "redensterkpunt" or key == "verbeterpunt" or key == "redenverbeterpunt" or key == "overall":
             details.get('Feedback').update(parameter)
+    
+    #Add question if answer is missing
+    if parameter.get('rolbeoordeelde') == '':
+        rolbeoordeeldetext = 'Wat is de rol van ', details.get('AlgemeneInformatie').get('beoordeelde'), '?'
+        return jsonify({'fulfillmentText': rolbeoordeeldetext})
+
+    if parameter.get('redensterkpunt') == '':
+        redensterkpunttext = 'U heeft aangegeven dat ', details.get('AlgemeneInformatie').get('beoordeelde'), ' goed is in ', details.get('Feedback').get('sterkpunt'), ', waarom vindt u dat?'
+        return jsonify({'fulfillmentText': redensterkpunttext})
+    
+    #Writing JSON file to pc
     with open('details.json', 'w') as json_file:
         json.dump(details, json_file)
     return details
